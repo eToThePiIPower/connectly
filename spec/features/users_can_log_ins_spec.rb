8c83ee459ea47e553ae8bf1590717af4f200203b
Charles Beynon <eToThePiIPower@gmail.com>
2018-08-15 16:46:10 -0400

    Add user devise model

--- spec/features/users_can_log_ins_spec.rb
+++ spec/features/users_can_log_ins_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.feature 'UsersCanLogIns', type: :feature do
   scenario 'User creats a new account' do
-    visit '/signup'
+    visit '/users/sign_up'
 
     fill_in 'Email', with: 'user@example.com'
     fill_in 'Password', with: 'password'
     fill_in 'Password confirmation', with: 'password'
-    click_button 'Sign up!'
+    click_button 'Sign up'
 
-    expect(page).to have_text('User created successfully')
+    expect(page).to have_text('You have signed up successfully')
   end
 end
