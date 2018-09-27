8267565c62a2ace3f2d23f3968e8815e9b51cf39
Charles Beynon <eToThePiIPower@gmail.com>
2018-09-27 15:56:11 -0400

    Update indices for associations and sorting

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_27_193130) do
+ActiveRecord::Schema.define(version: 2018_09_27_195459) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,8 @@ ActiveRecord::Schema.define(version: 2018_09_27_193130) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.uuid "user_id"
+    t.index ["created_at"], name: "index_contacts_on_created_at"
+    t.index ["user_id"], name: "index_contacts_on_user_id"
   end
 
   create_table "profiles", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
@@ -51,6 +53,8 @@ ActiveRecord::Schema.define(version: 2018_09_27_193130) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.uuid "user_id"
+    t.index ["created_at"], name: "index_profiles_on_created_at"
+    t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
   create_table "users", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
@@ -67,6 +71,7 @@ ActiveRecord::Schema.define(version: 2018_09_27_193130) do
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["created_at"], name: "index_users_on_created_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
