26f18494c1189d589acb4912aa36eeb10c2ca307
Charles Beynon <eToThePiIPower@gmail.com>
2018-09-27 15:52:34 -0400

    Enable database extension UUID

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_26_152136) do
+ActiveRecord::Schema.define(version: 2018_09_27_191603) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "uuid-ossp"
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
