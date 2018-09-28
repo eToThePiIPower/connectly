921afd7d077ca5cd61f13a278ca556af64e9e87a
Charles Beynon <eToThePiIPower@gmail.com>
2018-09-27 23:47:00 -0400

    Add .avatar? to Profile and delegate from User
    Just to make it simpler to determine if a profile has an avatar by
    eliminating one link in the dot chain. So instead of
    
    ```
    user.profile.avatar.attached?
    ```
    
    we can now call
    
    ```
    user.profile.avatar?
    ```
    
    Then we delegate the call from User to the Profile, leaving us with
    
    ```
    user.avatar?
    ```

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -3,4 +3,20 @@ require 'rails_helper'
 RSpec.describe Profile, type: :model do
   it { should belong_to :user }
   # it { should have_one_attached :avatar }
+
+  describe '.avatar?' do
+    it 'should return true when an avatar is attached' do
+      profile = create(:profile)
+      file = File.open(Rails.root.join('spec', 'factories', 'images', 'test.png'))
+      profile.avatar.attach(io: file, filename: 'test.png', content_type: 'image/png')
+
+      expect(profile.avatar?).to eq true
+    end
+
+    it 'should return false when an avatar is not attached' do
+      profile = create(:profile)
+
+      expect(profile.avatar?).to eq false
+    end
+  end
 end
