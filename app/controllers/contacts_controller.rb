37c6ae737f58355eb1b6f8acbedacdbe38b7cd0b
Charles Beynon <eToThePiIPower@gmail.com>
2018-10-02 00:23:01 -0400

    Fix errors in address fields on contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -78,7 +78,7 @@ class ContactsController < ApplicationController
   def contact_params
     params.require(:contact)
           .permit(:name, :email, :notes,
-            addresses_attributes: [:id, :address_type, :primary,
+            addresses_attributes: [:id, :address_type, :primary, :street1, :city, :state, :zip,
                                    body: [:street1, :city, :state, :zip]])
   end
 
